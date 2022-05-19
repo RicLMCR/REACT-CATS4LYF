c9c9b81089ff408d505be9bbd657736b330606d2
Leejjohno <leej67885@gmail.com>
2022-05-19 01:14:53 +0100

    general layout finalised

--- src/App.js
+++ src/App.js
@@ -3,16 +3,55 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="h1">
-      </header>
     
+    <div className="App">
+      <div className="container">
+        <h1>CATS4LYF</h1>
+          <div className="catBox">
+            <div className="catItem">Cat 1 Pic
+              
+              <div className="addToBasket">Add to basket!</div>
+            </div>
+            <div className="catItem">Cat 2 Pic
 
-      <div className="catBox">
-        <div className="catItem">
+              <div className="addToBasket">Add to basket!</div>
+            </div>
+            <div className="catItem">Cat 3 Pic
 
+              <div className="addToBasket">Add to basket!</div>
+            </div>
+            <div className="catItem">Cat 4 Pic
+
+              <div className="addToBasket">Add to basket!</div>
+            </div>
+            <div className="catItem">Cat 5 Pic
+
+              <div className="addToBasket">Add to basket!</div>
+            </div>
+            <div className="catItem">Cat 6 Pic
+
+              <div className="addToBasket">Add to basket!</div>
+            </div>
+            <div className="catItem">Cat 7 Pic
+
+              <div className="addToBasket">Add to basket!</div>
+            </div>
+            <div className="catItem">Cat 8 Pic
+
+              <div className="addToBasket">Add to basket!</div>
+            </div>
+            <div className="catItem">Cat 9 Pic
+
+              <div className="addToBasket">Add to basket!</div>
+            </div>
+          </div>
         </div>
-      </div>
+
+          <div className="basket">
+            <h2>Your Cats</h2>
+            <div>Items should list here</div>
+          </div>
+    
     </div>
   );
 }
